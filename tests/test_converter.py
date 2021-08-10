5292b8ce2a84ef8b6f198947055607b9acfc0b75
61Duke <61duke@isrc.iscas.ac.cn>
2021-08-10 09:22:31 +0800

    add converter class function

--- tests/test_converter.py
+++ tests/test_converter.py
@@ -9,3 +9,77 @@
 # PURPOSE.
 # See the Mulan PSL v2 for more details.
 # Create: 2021-7-13
+
+import sys
+
+sys.path.append("../")
+
+from geocoding import converter
+
+
+class TestConverter(object):
+    def test_gcj02_to_wgs84(self):
+        assert converter.convert(
+            geom=(
+                (116.403963, 39.915119),
+                (116.191704, 39.942046),
+                (121.394202, 31.172559)
+            ),
+            base="gcj02",
+            target="wgs84"
+        )
+
+    def test_wgs84_to_gcj02(self):
+        assert converter.convert(
+            geom=(
+                (116.403963, 39.915119),
+                (116.191704, 39.942046),
+                (121.394202, 31.172559)
+            ),
+            base="wgs84",
+            target="gcj02"
+        )
+
+    def test_bd09_to_gcj02(self):
+        assert converter.convert(
+            geom=(
+                (116.403963, 39.915119),
+                (116.191704, 39.942046),
+                (121.394202, 31.172559)
+            ),
+            base="bd09",
+            target="gcj02"
+        )
+
+    def test_gcj02_to_bd09(self):
+        assert converter.convert(
+            geom=(
+                (116.403963, 39.915119),
+                (116.191704, 39.942046),
+                (121.394202, 31.172559)
+            ),
+            base="gcj02",
+            target="bd09"
+        )
+
+    def test_bd09_to_wgs84(self):
+        assert converter.convert(
+            geom=(
+                (116.403963, 39.915119),
+                (116.191704, 39.942046),
+                (121.394202, 31.172559)
+            ),
+            base="bd09",
+            target="wgs84"
+        )
+
+    def test_wgs84_to_bd09(self):
+        assert converter.convert(
+            geom=(
+                (116.403963, 39.915119),
+                (116.191704, 39.942046),
+                (121.394202, 31.172559)
+            ),
+            base="wgs84",
+            target="bd09"
+        )
